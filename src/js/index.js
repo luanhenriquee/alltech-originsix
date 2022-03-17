6f13507b26e3012942dba9c14721347dccce03f8
prodigy07 <Luan_henrique21@hotmail.com>
2022-03-17 15:58:36 -0300

    ⛓ update js

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+const links = document.querySelectorAll('nav ul li a')
+
+for (const element of toggle) {
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
+}
+
+for (const link of links) {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show')
+  })
+}
